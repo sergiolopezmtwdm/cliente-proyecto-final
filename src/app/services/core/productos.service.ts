9320dd40782f325f9e582688e1612c3166b8e5b9
Sergio López <sergiolopez.mtwdm@gmail.com>
2020-12-11 05:40:48 -0600

    update product

--- src/app/services/core/productos.service.ts
+++ src/app/services/core/productos.service.ts
@@ -21,8 +21,8 @@ export class ProductosService {
   }
 
   /**
-   * 
-   * @param criterio 
+   *
+   * @param criterio
    */
   getGamesBycriterio(criterio: string) {
     return this.http.get(`${endPoint}/titulo/${criterio}`);
@@ -35,26 +35,40 @@ export class ProductosService {
     // return this.http.get("assets/json/producto.json");
   }
 
-  public updateProduct = (form: NgForm) => {
+  public updateProduct = (form: NgForm, id:number) => {
+    form.value.id = id;
+    form.value.costo = parseInt(form.value.costo);
+    form.value.precioVenta = parseInt(form.value.precioVenta);
+    form.value.idPlataforma = form.value.plataforma.value;
+    form.value.idGenero = form.value.genero.value;
+    form.value.idClasificacion = form.value.clasificacion.value;
+    delete form.value.plataforma;
+    delete form.value.genero;
+    delete form.value.clasificacion;
     const formulario = JSON.stringify(form.value);
     console.log("formulario: ", formulario);
+    this.http.post(`${endPoint}/update`,
+    formulario, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+      alert("modificación correcto");
+      // this.router.navigate(["/"]);
+      // this.router.navigate(["carrito"]);
 
-    // this.http.post(`${endPoint}/UpdateProduct`,
-    // formulario, {
-    //   headers: new HttpHeaders({
-    //     "Content-Type: "application/json"
-    //   })
-    // }).subscribe(response => {
-    //   alert("insert correcto");
-    // }, err => {
-    //   alert("fallo el insert");
-    // });
+      // this.sidebarSvc.getItems().subscribe((data:any)=>{
+      //   this.menuItems = data;
+      // });
 
+    }, err => {
+      alert("fallo el insert");
+      // this.invalidLogin = true;
+      // this.modalReference.close();
+    });
   }
 
   public insertProduct = (form: NgForm) => {
-
-
     // var producto: any = {
     //   sku: form.value.sku,
     //   titulo: form.value.titulo,
@@ -74,11 +88,11 @@ export class ProductosService {
     form.value.imagen = "";
     form.value.imagen2 = "";
     form.value.imagen3 = "";
-    form.value.urlVideo = "";
+    // form.value.urlVideo = "";
     form.value.idPlataforma = form.value.plataforma.value;
     form.value.idGenero = form.value.genero.value;
     form.value.idClasificacion = form.value.clasificacion.value;
-    form.value.edicion = "Standard";
+    // form.value.edicion = "Standard";
     delete form.value.plataforma;
     delete form.value.genero;
     delete form.value.clasificacion;
